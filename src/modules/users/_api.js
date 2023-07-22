cbcd92007e989885d5b653f9c93f516d845b2074
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-22 21:30:25 +0500

    post users postman orqali form-data versiyada ham o'zshadi

--- src/modules/users/_api.js
+++ src/modules/users/_api.js
@@ -1,7 +1,7 @@
 const { register_users } = require("./_controller");
-
+const multer = require("multer");
 const router = require("express").Router();
 
-router.post("/users", register_users);
+router.post("/users", multer().single(), register_users);
 
 module.exports = router;
