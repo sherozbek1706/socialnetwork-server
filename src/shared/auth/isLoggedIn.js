c23c8326081bf258ae58cf82b75efc74b4aa69dd
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-23 14:58:29 +0500

    user login qilganmi yo'qmi tekshirish

---
+++ src/shared/auth/isLoggedIn.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { UnauthorizedError } = require("../errors");
+const config = require("../config");
+const jwt = require("jsonwebtoken");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      throw new UnauthorizedError("Unauthorized.");
+    }
+
+    const decoded = jwt.verify(token, config.JWT.SECRET, {
+      ignoreExpiration: false,
+    });
+
+    req.user = decoded.user;
+  } catch (error) {
+    next(new UnauthorizedError(error.message));
+  }
+};
+
+module.exports = isLoggedIn;
