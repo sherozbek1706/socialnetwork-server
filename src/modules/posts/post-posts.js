edd8b997fb258930f4de464c8c2cf0bef0835cf8
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-23 15:58:03 +0500

    post qilganda userni malumotlari ko'rinadi

--- src/modules/posts/post-posts.js
+++ src/modules/posts/post-posts.js
@@ -1,7 +1,12 @@
 const Posts = require("./Posts");
 
 const postPosts = async ({ body, user_id, image }) => {
-  const newPost = await Posts.create({ ...body, user_id, image });
+  const newPost = (await Posts.create({ ...body, user_id, image })).populate([
+    {
+      path: "user_id",
+      select: "first_name last_name username",
+    },
+  ]);
 
   return newPost;
 };
