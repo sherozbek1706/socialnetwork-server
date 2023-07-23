6a39ec1c0d8e8c37b7df531400820371eeda5079
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-23 16:01:53 +0500

    like va timestamp qo'shildi

--- src/modules/posts/Posts.js
+++ src/modules/posts/Posts.js
@@ -1,16 +1,29 @@
 const mongoose = require("mongoose");
 
 const reqType = { type: mongoose.SchemaTypes.String, required: true };
-const postsSchemas = new mongoose.Schema({
-  title: reqType,
-  description: reqType,
-  image: reqType,
-  user_id: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Users",
+const postsSchemas = new mongoose.Schema(
+  {
+    title: reqType,
+    description: reqType,
+    image: reqType,
+    user_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Users",
+    },
+    web_link: reqType,
+    like: {
+      type: mongoose.SchemaTypes.Number,
+      default: 0,
+    },
   },
-  web_link: reqType,
-});
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const Posts = mongoose.model("Posts", postsSchemas);
 
